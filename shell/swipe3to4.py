666fdadc1fa27f00bef7b9b1eae0c2433afba484
icedman <m4rvin2005@gmail.com>
2021-05-06 11:28:18 +0800

    add custom.css

--- shell/swipe3to4.py
+++ shell/swipe3to4.py
@@ -3,6 +3,8 @@ s=f.read()
 f.close()
 s=s.replace(b'GESTURE_FINGER_COUNT\x20=\x203',b'GESTURE_FINGER_COUNT\x20=\x204')
 s=s.replace(b'Main.panel.style\x20=\x20\'tr',b'callback();\x20return;\x20//')
+#s=s.replace(b'/*\x20exported ControlsManager\x20*/',b'MUIG=imports.ui.main.uiGroup/')
+s=s.replace(b'MUIG.add_child(this.dash);',b'this.add_child(this.dash);')
 
 f=open("libgnome-shell-replaced.so","wb")
 f.write(s)
