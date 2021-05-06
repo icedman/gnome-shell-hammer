5ade04cf69163c662e13e985ce9f13fe658ae1ae
icedman <m4rvin2005@gmail.com>
2021-05-06 08:59:58 +0800

    add disable overview at startup

--- shell/swipe3to4.py
+++ shell/swipe3to4.py
@@ -2,6 +2,7 @@ f=open("/usr/lib64/gnome-shell/libgnome-shell.so","rb")
 s=f.read()
 f.close()
 s=s.replace(b'GESTURE_FINGER_COUNT\x20=\x203',b'GESTURE_FINGER_COUNT\x20=\x204')
+s=s.replace(b'Main.panel.style\x20=\x20\'tr',b'callback();\x20return;\x20//')
 
 f=open("libgnome-shell-replaced.so","wb")
 f.write(s)
