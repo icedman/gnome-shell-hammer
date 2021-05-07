05dcba5f38ce195cda5d7e05e5414570195e4caf
icedman <m4rvin2005@gmail.com>
2021-05-07 21:16:12 +0800

    add radius fix

--- shell/swipe3to4.py
+++ shell/swipe3to4.py
@@ -2,6 +2,7 @@ f=open("/usr/lib64/gnome-shell/libgnome-shell.so","rb")
 s=f.read()
 f.close()
 s=s.replace(b'GESTURE_FINGER_COUNT\x20=\x203',b'GESTURE_FINGER_COUNT\x20=\x204')
+s=s.replace(b'RADIUS_PIXELS\x20=\x2030',b'RADIUS_PIXELS\x20=\x2010')
 
 f=open("libgnome-shell-replaced.so","wb")
 f.write(s)
