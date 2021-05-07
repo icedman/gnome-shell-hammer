301e29e0b4076e49a9bf2911cd738c4d4b049a13
icedman <m4rvin2005@gmail.com>
2021-05-07 21:22:41 +0800

    add extension

--- shell/swipe3to4.py
+++ shell/swipe3to4.py
@@ -1,10 +1,13 @@
 f=open("/usr/lib64/gnome-shell/libgnome-shell.so","rb")
 s=f.read()
 f.close()
+
+# gesture
 s=s.replace(b'GESTURE_FINGER_COUNT\x20=\x203',b'GESTURE_FINGER_COUNT\x20=\x204')
-s=s.replace(b'Main.panel.style\x20=\x20\'tr',b'callback();\x20return;\x20//')
-#s=s.replace(b'/*\x20exported ControlsManager\x20*/',b'MUIG=imports.ui.main.uiGroup/')
-s=s.replace(b'MUIG.add_child(this.dash);',b'this.add_child(this.dash);')
+
+# overview startup animation
+# s=s.replace(b'Main.panel.style\x20=\x20\'tr',b'callback();\x20return;\x20//')
+s=s.replace(b'callback();\x20return;\x20//', b'Main.panel.style\x20=\x20\'tr')
 
 f=open("libgnome-shell-replaced.so","wb")
 f.write(s)
