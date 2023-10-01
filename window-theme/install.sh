f6693421e967a904d75b6d0d1baf7ed7bbfbb738
icedman <m4rvin2005@gmail.com>
2023-10-01 14:30:02 +0800

    use svg as backgroun images

--- window-theme/install.sh
+++ window-theme/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-mkdir -p ~/.config
 mkdir -p ~/.config/gtk-3.0
 mkdir -p ~/.config/gtk-4.0
 cp ./gtk-4.0/gtk.css ~/.config/gtk-4.0/gtk.css
 cp ./gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+cp ./*.svg ~/.config/gtk-4.0/
+cp ./*.svg ~/.config/gtk-3.0/
